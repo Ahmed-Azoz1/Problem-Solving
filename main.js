9c7fb49b836a4a16b068bd1df9356c639b6bd72a
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-09 16:52:38 +0200

    Sum Arrays

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function even_or_odd(number){
             return "Odd"
         }
 }
+
+// 2-Sum Arrays
+function sum (numbers) {
+    let initValue = 0;
+    for(let i=0;i<numbers.length;i++){
+    initValue += numbers[i]
+    }
+    return initValue
+};
