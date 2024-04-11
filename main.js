3288fa832e074c98ed3d7b01b8928672bdb4d45b
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-11 21:25:41 +0200

    Rock Paper Scissors!

--- main.js
+++ main.js
@@ -73,3 +73,29 @@ function smash (words) {
     // }
     // return res.trim();   
 };
+
+
+// 9-Rock Paper Scissors!
+const rps = (p1, p2) => {
+    if(p1 === p2){
+        return 'Draw';
+    }else if(p1 == 'rock' && p2 == 'scissors'){
+        return 'Player 1 won'
+    }else if(p1 == 'paper' && p2 =='rock'){
+        return 'Player 1 won'
+    }else if(p1 == 'scissors' && p2 =='paper'){
+        return 'Player 1 won'
+    }else{
+        return 'Player 2 won'
+    }
+
+    // or
+    // if (p1 === p2) return "Draw!";
+    // var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    // if (p2 === rules[p1]) {
+    //     return "Player 1 won!";
+    // }
+    // else {
+    //     return "Player 2 won!";
+    // }
+};
