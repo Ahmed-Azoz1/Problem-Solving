b255545bf86c2efd331eb0d659913f260ce5225c
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-13 20:38:29 +0200

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

--- main.js
+++ main.js
@@ -142,3 +142,22 @@ function maps(x){
     // }
     // return arr;
 }
+
+
+// 13-Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+function replace(s){
+    //coding and coding....
+    return s.replace(/[aeoiu]/ig, '!') 
+
+    // or
+    // let newS = ''
+    // let vowels = "aAeEiIoOuU"
+    // for (let i = 0; i < s.length; i++) {
+    //     if (vowels.indexOf(s[i]) === -1) {
+    //     newS += s[i]
+    //     }  else {
+    //     newS += '!'
+    //     }
+    // }
+    // return newS
+}
