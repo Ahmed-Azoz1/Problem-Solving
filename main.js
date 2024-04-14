85b357c201e33d263128ced0f535fdc5b11c64f2
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-14 20:54:54 +0200

    All Star Code Challenge #18

--- main.js
+++ main.js
@@ -180,3 +180,16 @@ function invert(array) {
     // }
     // return newArr;
 }
+
+// 16-All Star Code Challenge #18
+function strCount(str, letter){  
+    let num = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == letter){
+            num++;
+        }
+    }
+    return num;
+    // or
+    // return str.split('').filter(c => c == letter).length;
+}
