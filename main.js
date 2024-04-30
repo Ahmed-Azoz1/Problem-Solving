0ef7b422841033e20126dd3253f206eb7f0f1ac8
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-30 20:30:28 +0300

    Simple Change Machine

--- main.js
+++ main.js
@@ -417,3 +417,33 @@ function monkeyCount(n) {
     // }
     // return monkeys;
 }
+
+// 32-Simple Change Machine
+function changeMe(moneyIn){
+    switch(moneyIn){
+        case '£5':return '20p '.repeat(25).trim()
+        case '£2':return '20p '.repeat(10).trim()
+        case '£1':return '20p '.repeat(5).trim()
+        case '50p':return '20p 20p 10p'
+        case '20p':return '10p 10p'
+        default:return moneyIn;
+    }
+    //or
+    // switch (moneyIn) {
+    //     case '£5':
+    //         var change = Array(25).fill('20p');
+    //         return change.join(' ');
+    //     case '£2':
+    //         var change = Array(10).fill('20p');
+    //         return change.join(' ');
+    //     case '£1':
+    //         var change = Array(5).fill('20p');
+    //         return change.join(' ');
+    //     case '50p':
+    //         return '20p 20p 10p';
+    //     case '20p':
+    //         return '10p 10p';
+    //     default:
+    //         return moneyIn;
+    // }
+}
