c790ecba4193df764893cf51280a55e0b7c934f3
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-05-04 20:58:17 +0300

    Points of Reflection

--- main.js
+++ main.js
@@ -469,3 +469,12 @@ function fixTheMeerkat(arr) {
     // }
     // return nArr;
 }
+
+// 35-Points of Reflection
+function symmetricPoint(p, q) {
+    return [2*q[0] - p[0],2*q[1] -p[1]]
+    // or
+    // const p1 = (q[0] * 2) - p[0]
+    // const p2 = (q[1] * 2) - p[1]
+    // return [p1, p2]
+}
