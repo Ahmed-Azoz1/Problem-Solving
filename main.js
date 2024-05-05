2c35ff3da6656bee0278177018bebde8628eb477
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-05-05 06:01:42 +0300

    Days in the year

--- main.js
+++ main.js
@@ -478,3 +478,18 @@ function symmetricPoint(p, q) {
     // const p2 = (q[1] * 2) - p[1]
     // return [p1, p2]
 }
+
+// 36-Days in the year
+function yearDays(year) {
+    return year + ' has ' + (!(year % 100) && year % 400 || year % 4 ? '365' : '366') + ' days';
+    // or
+    // if (year % 4 !== 0) {
+    //     return `${year} has 365 days`;
+    // } else if (year % 100 !== 0) {
+    //     return `${year} has 366 days`;
+    // } else if (year % 400 !== 0) {
+    //     return `${year} has 365 days`;
+    // } else {
+    //     return `${year} has 366 days`;
+    // }
+}
