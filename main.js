380670484001b6627bed97d3a5493347d94feea3
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-05-11 10:28:46 +0300

    Beginner Series #1 School Paperwork

--- main.js
+++ main.js
@@ -544,3 +544,11 @@ function grow(x){
     // }
     // return res;
 }
+
+// 42-Beginner Series #1 School Paperwork
+function paperwork(n, m) {
+    if(n<0||m<0) return 0;
+    return n * m;
+    // or
+    // return n < 0 || m < 0 ? 0 : n * m;
+}
