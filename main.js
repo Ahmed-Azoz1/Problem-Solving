d6bb30650630a3f3c00de52c6cea87fa55b7237e
Ahmed-ElQaNaS <elqanasahmed2@gmail.com>
2024-07-29 10:23:43 +0300

    Return the day

--- main.js
+++ main.js
@@ -1320,3 +1320,27 @@ function expressionMatter(a, b, c) {
     // let values = [(a*(b+c)), (a*b*c), (a+b*c), ((a+b)*c), (a+b+c)];
     // return Math.max(...values);
 }
+
+// 115-Return the day
+function whatday(num) { 
+    switch(num) {
+        case 1:
+            return "Sunday";
+        case 2:
+            return "Monday";
+        case 3:
+            return "Tuesday";
+        case 4:
+            return "Wednesday";
+        case 5:
+            return "Thursday";
+        case 6:
+            return "Friday";
+        case 7:
+            return "Saturday";
+        default:
+            return 'Wrong, please enter a number between 1 and 7';
+        }
+    // or
+    // return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][num -1] || "Wrong, please enter a number between 1 and 7";
+}
