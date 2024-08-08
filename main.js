db702b3ca5c95c07f4476d44648e592a5a31c34b
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-08-08 13:44:55 +0300

    Leonardo Dicaprio and Oscars

--- main.js
+++ main.js
@@ -1467,3 +1467,23 @@ const templateStrings = (noun, adjective) =>{
     // or
     // return noun + " are " + adjective;
 }
+
+// 127-Leonardo Dicaprio and Oscars
+function leo(oscar){
+    if(oscar == 88 ){
+        return "Leo finally won the oscar! Leo is happy";
+    }else if(oscar == 86){
+        return "Not even for Wolf of wallstreet?!";
+    }else if(oscar < 88){
+        return "When will you give Leo an Oscar?";
+    }else{
+        return "Leo got one already!";
+    }
+    // or
+    // const responses = {
+    //     86: "Not even for Wolf of wallstreet?!",
+    //     87: "When will you give Leo an Oscar?",
+    //     88: "Leo finally won the oscar! Leo is happy"
+    // };
+    // return responses[oscar] || (oscar > 88 ? "Leo got one already!" : "When will you give Leo an Oscar?");
+}
