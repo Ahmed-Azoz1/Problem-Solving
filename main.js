f9f26e26cafe2374a57c55d1f84997e7306123de
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-08-13 04:24:34 +0300

    Heads and Legs

--- main.js
+++ main.js
@@ -1522,3 +1522,17 @@ function remove (string) {
     // or
     // return string.charAt(string.length - 1) === '!' ? remove(string.slice(0, string.length - 1)) : string;
 }
+
+// 132-Heads and Legs
+function animals(heads, legs){
+    let cows = legs%2 ? -1 : legs/2-heads;
+    return heads >= cows && cows >= 0 ? [heads-cows, cows] : "No solutions";
+    // or
+    // let cow = (legs - heads * 2) / 2;
+    // let chicken = heads - (legs - heads * 2) / 2;
+    // if (chicken < 0 || cow < 0 || Math.round(chicken) != chicken || Math.round(cow) != cow){
+    //     return "No solutions";
+    // }else{
+    //     return [chicken, cow];
+    // }
+}
