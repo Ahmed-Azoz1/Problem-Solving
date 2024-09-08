c2504cc7f9dd90e88c7277ab0a87ede8b87187e5
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-09-08 14:11:16 +0300

    Welcome!

--- main.js
+++ main.js
@@ -1806,3 +1806,47 @@ function squaresNeeded(grains){
     // or
     // return Math.ceil(Math.log2(grains+1))
 }
+
+// 157-Welcome!
+function greet(language) {
+	return((hello)=>hello[language] || 'Welcome')({
+        czech: "Vitejte"
+        , danish: "Velkomst"
+        , dutch: "Welkom"
+        , estonian: "Tere tulemast"
+        , finnish: "Tervetuloa"
+        , flemish: "Welgekomen"
+        , french: "Bienvenue"
+        , german: "Willkommen"
+        , irish: "Failte"
+        , italian: "Benvenuto"
+        , latvian: "Gaidits"
+        , lithuanian: "Laukiamas"
+        , polish: "Witamy"
+        , spanish: "Bienvenido"
+        , swedish: "Valkommen"
+        , welsh: "Croeso"
+    }
+    )
+    // or
+    // switch(language){
+    //     case "english": return "Welcome";
+    //     case "czech": return "Vitejte";
+    //     case "danish": return "Velkomst";
+    //     case "dutch": return "Welkom";
+    //     case "estonian": return "Tere tulemast";
+    //     case "finnish": return "Tervetuloa";
+    //     case "flemish": return "Welgekomen";
+    //     case "french": return "Bienvenue";
+    //     case "german": return "Willkommen";
+    //     case "irish": return "Failte";
+    //     case "italian": return "Benvenuto";
+    //     case "latvian": return "Gaidits";
+    //     case "lithuanian": return "Laukiamas";
+    //     case "polish": return "Witamy";
+    //     case "spanish": return "Bienvenido";
+    //     case "swedish": return "Valkommen";
+    //     case "welsh": return "Croeso";
+    //     default: return "Welcome";
+    // }
+}
