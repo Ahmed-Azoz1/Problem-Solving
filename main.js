5fb367b2d71960d136dd2fc96d0bb8dddda241a5
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-10-16 11:00:34 +0300

    Fix your code before the garden dies!

--- main.js
+++ main.js
@@ -2307,3 +2307,14 @@ function sameCase(a, b){
     //     return 1;
     // } else return 0;
 }
+
+// 196-Fix your code before the garden dies!
+function rainAmount(mm){
+    if (mm < 40) {
+        return `You need to give your plant ${40 - mm}mm of water`;
+    }else{
+        return `Your plant has had more than enough water for today!`;
+    };
+    // or
+    // return mm < 40 ? `You need to give your plant ${40 - mm}mm of water` : "Your plant has had more than enough water for today!";
+}
