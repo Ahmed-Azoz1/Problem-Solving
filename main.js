20d2ff7c0aca7f0b40f5e1b2e475c080f65364e1
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-10-27 17:07:19 +0300

    Calculate BMI

--- main.js
+++ main.js
@@ -2413,3 +2413,20 @@ function simpleMultiplication(number) {
     //     return number * 9;
     // } 
 }
+
+// 207-Calculate BMI
+function bmi(weight, height) {
+    let bmi = weight / height **2;
+    if(bmi <= 18.5){
+        return 'Underweight';
+    }else if(bmi <= 25){
+        return 'Normal';
+    }else if(bmi <= 30){
+        return 'Overweight';
+    }else{
+        return 'Obese';
+    }
+    // or
+    // let bmi = weight / height**2;
+    // return bmi <= 18.5 ? 'Underweight' : bmi <= 25.0 ? 'Normal' : bmi <= 30.0 ? 'Overweight' : 'Obese';
+}
