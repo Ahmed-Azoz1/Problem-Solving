a778b2e18b1fe854ec9a38bd6a1407dde926eb06
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-12-10 08:10:27 +0200

    Fun with ES6 Classes #1 - People, people, people

--- main.js
+++ main.js
@@ -2951,3 +2951,29 @@ class Cube {
     //     this.side = n ? Math.abs(n) : 0
     // }
 }
+
+// 251-Fun with ES6 Classes #1 - People, people, people
+class Person {
+    constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+    sayFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    static greetExtraTerrestrials(raceName) {
+        return `Welcome to Planet Earth ${raceName}`;
+    }
+    // or
+    // constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    //     Object.assign(this, { firstName, lastName, age, gender });
+    // }
+    // sayFullName() {
+    //     return `${this.firstName} ${this.lastName}`;
+    // }
+    // static greetExtraTerrestrials(raceName) {
+    //     return `Welcome to Planet Earth ${raceName}`;
+    // }
+}
