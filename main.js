f76220150f153683a9b84b5e60f1634fafabc1fa
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2025-02-14 17:07:45 +0200

    Training JS #10: loop statement --for

--- main.js
+++ main.js
@@ -3634,3 +3634,20 @@ function greet(name){
     // or
     // return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
 }
+
+// 313-Training JS #10: loop statement --for
+function pickIt(arr){
+    let odd = [], even = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]%2){
+            odd.push(arr[i])
+        }else{
+            even.push(arr[i])
+        }
+    }
+    return [odd,even];
+    // // or
+    // let odd = [], even = [];
+    // for (let num of arr) num & 1 ? odd.push(num) : even.push(num);
+    // return [odd,even];
+}
