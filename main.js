63bf6447b3a13c9fe1be5b98cf8715f3295732d9
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2025-03-26 20:03:18 +0200

    Grasshopper - Object syntax debug

--- main.js
+++ main.js
@@ -4092,3 +4092,21 @@ function firstToLast(str,c){
     // let last = str.lastIndexOf(c);
     // return first === -1 ? -1 : last - first;
 }
+
+// 354-Grasshopper - Object syntax debug
+let rooms = {
+    first: {
+        description: 'This is the first room',
+        items: {
+            chair: 'The old chair looks comfortable',
+            lamp: 'This lamp looks ancient'
+        }
+    },
+    second: {
+        description: 'This is the second room',
+        items: {
+            couch: 'This couch looks like it would hurt your back',
+            table: 'On the table there is an unopened bottle of water'
+        },
+    }
+}
