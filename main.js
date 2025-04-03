cecc0b4b6814d32eee6f51e7552f563bb06c273b
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2025-04-03 07:55:45 +0200

    Blood-Alcohol Content

--- main.js
+++ main.js
@@ -4178,3 +4178,19 @@ function isOpposite(s1,s2){
     // }
     // return true;
 }
+
+// 362-Blood-Alcohol Content
+function bloodAlcoholContent(drinks, weight, sex, time){
+    const r = sex === 'male' ? 0.73 : 0.66
+    const bac = drinks.ounces * drinks.abv * 5.14 / weight * r - 0.015 * time;
+    return Number(bac.toFixed(4));
+    // or
+    // let r = 0;
+    // if (sex == 'male'){
+    //     r = 0.73;
+    // }else{
+    //     r = 0.66;
+    // }
+    // let bac = (drinks.ounces * drinks.abv * 5.14 / weight * r) - 0.015 * time;
+    // return +bac.toFixed(4);
+}
